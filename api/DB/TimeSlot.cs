60a9b3ef2ca615dd875bb8c644488bd8b18f4905
BogdanYarotsky <byarotsky@gmail.com>
2023-12-23 08:16:55 +0100

    Scaffolding cs projects

--- api/DB/TimeSlot.cs
+++ api/DB/TimeSlot.cs
@@ -2,34 +2,30 @@ namespace API.DB;
 
 using System.Collections.Generic;
 
-public class TimeSlot
+internal class TimeSlot
 {
     public int Id { get; set; }
-
     public int EventId { get; set; }
     public int MachineId { get; set; }
     public DateTime Start { get; set; }
     public DateTime End { get; set; }
-
-    public Event Event { get; set; } = null!;
-    public Machine Machine { get; set; } = null!;
 }
 
 // get all slots for machine in time range
 
-public abstract class Event
+internal abstract class Event
 {
     public int Id { get; set; }
     public ICollection<TimeSlot> TimeSlots { get; set; }
 }
 
-public class Standstill : Event
+internal class Standstill : Event
 {
     public StandstillReason Reason { get; set; }
     public int Priority { get; set; }
 }
 
-public class StandstillReason
+internal class StandstillReason
 {
     public int Id { get; set; }
 
@@ -40,7 +36,7 @@ public class StandstillReason
     public ICollection<StandstillReason> Children { get; set; } = null!;
 }
 
-public class Machine
+internal class Machine
 {
     public int Id { get; set; }
     public ICollection<TimeSlot> TimeSlots { get; set; }
